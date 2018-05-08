1dcb194b3e3816ca6ac231cd722626a8b31fa557
Frank Kleine <mikey@bovigo.org>
2018-05-08 17:14:19 +0200

    fix typo

--- error_nocontext.go
+++ error_nocontext.go
@@ -6,7 +6,7 @@ import (
 func readConfig(configFile string) (*Config, error) {
 	f, err := os.Open(configFile)
 	if err != nil {
-		return nil err
+		return nil, err
 	}
 	// do something with the open *File f and return Config
 }
